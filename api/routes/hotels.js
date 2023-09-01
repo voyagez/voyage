19c66bc953d832d51328bbcd588df15af223a372
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2023-09-01 11:52:07 -0400

    Presentation

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -10,16 +10,16 @@ import {
   updateHotel,
 } from "../controllers/hotel.js";
 import Hotel from "../models/Hotel.js";
-import {verifyAdmin} from "../utils/verifyToken.js"
+import { verifyAdmin } from "../utils/verifyToken.js"
 const router = express.Router();
 
 //CREATE
-router.post("/", verifyAdmin, createHotel);
+router.post("/", createHotel);
 
 //UPDATE
-router.put("/:id", verifyAdmin, updateHotel);
+router.put("/:id", updateHotel);
 //DELETE
-router.delete("/:id", verifyAdmin, deleteHotel);
+router.delete("/:id", deleteHotel);
 //GET
 
 router.get("/find/:id", getHotel);
